5423f9b226a94b3673742b28118d4cbb97d87204
SangaviT <tsangavi189@gmail.com>
2016-08-13 12:50:59 +0530

    Add files via upload

---
+++ prog1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+
+
+int main() 
+{	
+	
+int n;
+	
+scanf("%d",&n);
+	
+
+if(n>=1)
+	
+printf("Positive");
+	
+else if(n==0)
+	
+printf("Zero");
+	
+else
+	
+printf("Negative");
+	
+return 0;
+
+}
